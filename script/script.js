d02cc61f9f1de5aca9731d46143b7177c50a883e
2ndBrigadeWebProgrammer <kmchoi28@naver.com>
2020-12-19 13:07:21 +0000

    container sync

---
+++ script/script.js
@@ -0,0 +1,7 @@
+
+var welc = document.getElementsByClassName('welcome-box');
+
+function newCont() {
+	var cont = document.getElementsByClassName('content');
+	cont.innerHTML="/html/"+this.value+".html";
+}
