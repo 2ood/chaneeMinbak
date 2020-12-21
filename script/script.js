7b7e521d262df99bfe841324bda5ed8da0fcdc96
2ndBrigadeWebProgrammer <kmchoi28@naver.com>
2020-12-21 14:51:02 +0000

    made intro page

--- script/script.js
+++ script/script.js
@@ -1,3 +1,10 @@
+var parts = window.location.search.substr(1).split("&");
+var $_GET = {};
+for (var i = 0; i < parts.length; i++) {
+    var temp = parts[i].split("=");
+    $_GET[decodeURIComponent(temp[0])] = decodeURIComponent(temp[1]);
+}
+
 var welc = document.getElementsByClassName('welcome-box');
 
 function newCont(th) {
@@ -18,3 +25,12 @@ function fetchPage(name){
       })
     });
   }
+
+const button = document.querySelector('.search-reserv');
+button.onclick = checkBook();
+
+function checkBook() {
+	if(($_GET['trip-start']!=null) || ($_GET['trip-stop']!=null) ) {fetchPage('html/book.html');} 
+else ;	
+}
+
