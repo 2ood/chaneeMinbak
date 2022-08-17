7964fe17c224c17971c95457ef6ab5e22e48983f
2ood <kyungmin.official0@gmail.com>
2022-08-18 00:23:18 +0900

    refactor : big refactring

--- script/script.js
+++ script/script.js
@@ -1,95 +1,65 @@
 // use GET variables from URL
 var parts = window.location.search.substr(1).split("&");
-var $_GET = {};
-for (var i = 0; i < parts.length; i++) {
-    var temp = parts[i].split("=");
-    $_GET[decodeURIComponent(temp[0])] = decodeURIComponent(temp[1]);
-}
 
-// make variables from html components
-const home = document.getElementsByClassName('home');
-const bookCheck = document.querySelector('.search-reserv');
-const prevButton = document.querySelector('.change-prev');
-const nextButton = document.querySelector('.change-next');
-const hamburger = document.querySelector('.hamburger');
+window.addEventListener("load",function(evt){
+  const home = document.getElementsByClassName('home');
+  const bookCheck = document.querySelector('.search-reserv');
+  const prevButton = document.getElementById('change-prev');
+  const nextButton = document.getElementById('change-next');
+  const ham = document.getElementById('ham');
+  ham.addEventListener("click",openHam);
+  prevButton.addEventListener("click",changePic);
+  nextButton.addEventListener("click",changePic);
+});
 
-//assign onclick function
-bookCheck.onclick = checkBook(true);
-//prevButton.setAttribute("onclick", "changePic()");
-//nextButton.onclick = changePic();
-
-function fetchPage(name){
-    fetch(name).then(function(response){
-      response.text().then(function(text){
-        document.querySelector('.content').innerHTML = text;
-      })
-    });
-  }
-
-function fetchImage(div, name) {
-	fetch(name).then(function(response){
-      response.text().then(function(text){
-        document.querySelector(div).backgroundImage=text;
-      })
-    });
-  }
-
-function checkBook(flag) {
-	if(($_GET['trip-start']!=null) || ($_GET['trip-stop']!=null) )
-		fetchPage('html/book.html');
-else ;	
-}
-
-function openHam() {
-	console.log('openham');
+function openHam(evt) {
 	var nonhomes = document.querySelector('.container-nonhomes');
 	let flag = Number(nonhomes.getAttribute('name'));
-	console.log(flag);
-	if (flag===0) { nonhomes.setAttribute('style','display:inline-block;'); nonhomes.setAttribute('name',1); }
-	else {nonhomes.setAttribute('style','display:none;');nonhomes.setAttribute('name',0); }
-}
-function changePic(flag) {
-	var welcbackg = document.querySelectorAll('.welcome-box img');
-	var welc = document.querySelector('.welcome-box');
-	console.log('clicked'+flag);
-	
-	let status =Number(welc.getAttribute('name'));
-	//console.log(s);
-	let index;
-	let len= welcbackg.length;
-	if(flag) {
-		if(len===status+1) index=0;
-		else index = status+1;
-	}
-	else {
-		if(status===0) index=len-1;
-		else index = status-1;
-	}
-	
-	let j=0;
-	for(; j<len ;  j++) {
-		if (j===index) welcbackg[j].setAttribute('style',"display:inline;");
-		else welcbackg[j].setAttribute('style',"display:none;");
-	}
-	welc.setAttribute('name',index);
-	
 }
 
+function changePic(evt) {
+  const src = evt.srcElement;
+  let flag = (src.id=="change-next");
 
+  const scroll_banner = document.querySelector("banner-container");
+  let current_slide = scroll_banner.classList[0];
 
-$(window).scroll(function() {
-	var btn = $('#to-the-top');
-  if ($(window).scrollTop() > 3) {
-    btn.addClass('show');
+  if(flag) {
+    switch(current_slide){
+      case 'first' : {
+        scroll_banner.classList.remove("first");
+        scroll_banner.classList.add("second");
+        break;
+      }
+      case 'second' : {
+        scroll_banner.classList.remove("second");
+        scroll_banner.classList.add("third");
+        break;
+      }
+      default : {
+        scroll_banner.classList.remove("third");
+        scroll_banner.classList.add("first");
+        break;
+      }
+    }
   } else {
-    btn.removeClass('show');
+    switch(current_slide){
+      case 'first' : {
+        scroll_banner.classList.remove("first");
+        scroll_banner.classList.add("third");
+        break;
+      }
+      case 'second' : {
+        scroll_banner.classList.remove("second");
+        scroll_banner.classList.add("first");
+        break;
+      }
+      default : {
+        scroll_banner.classList.remove("third");
+        scroll_banner.classList.add("second");
+        break;
+      }
+    }
   }
-});
 
-btn.on('click', function(e) {
-  e.preventDefault();
-  $('html, body').animate({scrollTop:0}, '300');
-});
-
-
-
+}
